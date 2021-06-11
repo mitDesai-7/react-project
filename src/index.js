48530f82c324c7da36f4874292b2025413df7aa8
Mit Desai <mit.util.access@gmail.com>
2021-06-11 16:44:29 -0700

    images added

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import SearchMovie from './searchMovie'
 
 ReactDOM.render(
   <div className="container">
-    <h1 className="title">Welcome to Movie World</h1>
-    <SearchMovie/>
+    <h1 className="title">Welcome to MD's Movie World</h1>
+    <SearchMovie />
   </div>,
   document.getElementById('root')
 );
